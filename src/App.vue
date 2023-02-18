d88342a97f95cddec315fc7c9a318eed5b48d78e
takumi-13 <takumi.gameover13@gmail.com>
2023-02-18 13:14:19 +0900

    サンプルの追加

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <script setup lang="ts">
   import Test1 from '@/components/Test1.vue';
   import Test2 from '@/components/Test2.vue';
+  import { ref } from 'vue';
+  const countTest1 = ref(2);
+  const setCountTest1 = (newValue: number): number => (countTest1.value = newValue);
 </script>
 
 <template>
   <div>Hello,World</div>
-  <Test1 msg="Test1" />
+  <Test1 msg="Test1" :count="countTest1" :set-count="setCountTest1" />
   <Test2 msg="Test2" />
 </template>
 
--- src/components/Test1.vue
+++ src/components/Test1.vue
@@ -1,12 +1,34 @@
-<script setup lang="ts">
-  const props = defineProps<{
-    msg: string;
-  }>();
+<script lang="ts">
+  import { defineComponent } from 'vue';
+  import type { PropType } from 'vue';
+  export default defineComponent({
+    props: {
+      count: {
+        type: Number,
+        required: true,
+      },
+      msg: {
+        type: String,
+        required: true,
+      },
+      setCount: {
+        type: Function as PropType<(arg: number) => number>,
+        required: true,
+      },
+    },
+    setup(props) {
+      return { props };
+    },
+  });
 </script>
 
 <template>
   <div>
-    {{ props.msg }}
+    <h1>
+      {{ props.msg }}
+    </h1>
+    <h1>{{ props.count }}</h1>
+    <button @click="() => props.setCount(props.count + 1)">add Test1</button>
   </div>
 </template>
 
